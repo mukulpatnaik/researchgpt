39d8da6939ab723e36bbf821a6a6ee07060b5bbf
MrPeterJin <Cheng.Jin@std.uestc.edu.cn>
2023-03-04 15:12:12 +0800

    Rebase the original repo

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # ResearchGPT
 
+This is a fork of the original [ResearchGPT](https://github.com/mukulpatnaik/researchgpt). I use it to study OpenAI's API and Microsoft's Azure. Partial original README is below.
+
 This is a flask app provides an interface to enable a conversation with a research paper. You can enter a link to a pdf hosted online or upload your own pdf. The app will then extract the text from the pdf, create embeddings from the text and use them with the openai api to generate a response to a question you ask. It will also return a source for the part of the text it used to generate the response and the page number. 
 
-Try the demo at: https://researchgpt.ue.r.appspot.com/
 
 ## Example 
 
@@ -11,30 +12,32 @@ https://user-images.githubusercontent.com/36257370/218764852-32b79201-4767-4684-
 ## Installation
 
 ```bash
-git clone https://github.com/mukulpatnaik/researchgpt.git
+git clone https://github.com/MrPeterJin/researchgpt
 cd researchgpt
 pip install -r requirements.txt
 ```
 
+Also, you need to specify your OpenAI API key in the variable `openai.api_key` of `app.py`.
+
 ## Usage
 
-You need to have an openai api key and set it as the environment variable 'OPENAI_API_KEY'.
+### Run locally
 
 ```bash
-python main-local.py
+python app.py
 ```
 
-## Google Cloud Deployment
-
-Follow the instructions here: https://cloud.google.com/appengine/docs/standard/python3/building-app/deploying-web-service
-Once you have the app.yaml file set up with your openai key and also have gcloud cli set up, you can deploy with:
+or:
 
 ```bash
-gcloud app deploy
+flask run
 ```
 
-To stream logs:
+### Microsoft Azure Deployment
+
+Follow the instructions [here](https://learn.microsoft.com/zh-cn/azure/app-service/quickstart-python?tabs=flask%2Cwindows%2Cazure-cli%2Cvscode-deploy%2Cdeploy-instructions-azportal%2Cterminal-bash%2Cdeploy-instructions-zip-azcli)
+Once you have the azure cli set up with `az login`, you can deploy with streamed logs:
 
 ```bash
-gcloud app logs tail
+az webapp up --runtime PYTHON:3.9 --sku B1 --logs
 ```
--- app.py
+++ app.py
@@ -7,19 +7,16 @@ import openai
 import os
 import requests
 from flask_cors import CORS
-from _md5 import md5
-from google.cloud import storage
+
+openai.api_key='sk-' # Your OpenAI API key
 
 app = Flask(__name__)
-# db=redis.from_url(os.environ['REDISCLOUD_URL'])
-# db = redis.StrictRedis(host='localhost', port=6379, db=0)
-# os.environ['CLOUD_STORAGE_BUCKET'] = 'researchgpt.appspot.com'
-CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
 CORS(app)
+
 
 class Chatbot():
     
-    def extract_text(self, pdf):
+    def parse_paper(self, pdf):
         print("Parsing paper")
         number_of_pages = len(pdf.pages)
         print(f"Total number of pages: {number_of_pages}")
@@ -71,7 +68,7 @@ class Chatbot():
         # print(paper_text)
         return paper_text
 
-    def create_df(self, pdf):
+    def paper_df(self, pdf):
         print('Creating dataframe')
         filtered_pdf= []
         for row in pdf:
@@ -86,16 +83,15 @@ class Chatbot():
         print('Done creating dataframe')
         return df
 
-    def embeddings(self, df):
+    def calculate_embeddings(self, df):
         print('Calculating embeddings')
-        openai.api_key = os.getenv('OPENAI_API_KEY')
         embedding_model = "text-embedding-ada-002"
         embeddings = df.text.apply([lambda x: get_embedding(x, engine=embedding_model)])
         df["embeddings"] = embeddings
         print('Done calculating embeddings')
         return df
 
-    def search(self, df, query, n=3, pprint=True):
+    def search_embeddings(self, df, query, n=3, pprint=True):
         query_embedding = get_embedding(
             query,
             engine="text-embedding-ada-002"
@@ -114,7 +110,7 @@ class Chatbot():
         return results.head(n)
     
     def create_prompt(self, df, user_input):
-        result = self.search(df, user_input, n=3)
+        result = self.search_embeddings(df, user_input, n=3)
         print(result)
         prompt = """You are a large language model whose expertise is reading and summarizing scientific papers. 
         You are given a query and a series of text embeddings from a paper in order of their cosine similarity to the query.
@@ -135,107 +131,57 @@ class Chatbot():
 
     def gpt(self, prompt):
         print('Sending request to GPT-3')
-        openai.api_key = os.getenv('OPENAI_API_KEY')
         r = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0.4, max_tokens=1500)
         answer = r.choices[0]['text']
         print('Done sending request to GPT-3')
         response = {'answer': answer, 'sources': sources}
         return response
 
+    def reply(self, prompt):
+        print(prompt)
+        prompt = self.create_prompt(df, prompt)
+        return self.gpt(prompt)
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
-
 @app.route("/process_pdf", methods=['POST'])
 def process_pdf():
     print("Processing pdf")
-    print(request)
-    # print('the data')
-    # print(request.data)
     file = request.data
-
-    key = md5(file).hexdigest()
-    print(key)
-    # Create a Cloud Storage client.
-    gcs = storage.Client()
-    name = key+'.json'
-
-    # Get the bucket that the file will be uploaded to.
-    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-    # Check if the file already exists
-    if bucket.blob(name).exists():
-        print("File already exists")
-        print("Done processing pdf")
-        return {"key": key}
-
     pdf = PdfReader(BytesIO(file))
     chatbot = Chatbot()
-    paper_text = chatbot.extract_text(pdf)
-    df = chatbot.create_df(paper_text)
-    df = chatbot.embeddings(df)
-    
-    # Create a new blob and upload the file's content.
-    blob = bucket.blob(name)
-    blob.upload_from_string(df.to_json(), content_type='application/json')
-    # if db.get(key) is None:
-    #     db.set(key, df.to_json())
+    paper_text = chatbot.parse_paper(pdf)
+    global df
+    df = chatbot.paper_df(paper_text)
+    df = chatbot.calculate_embeddings(df)
     print("Done processing pdf")
-    return {"key": key}
+    return {'key': ''}
 
 @app.route("/download_pdf", methods=['POST'])
 def download_pdf():
-    print("Downloading pdf")
-    print(request)
-    print(request.json['url'])
     chatbot = Chatbot()
     url = request.json['url']
-    r = requests.get(str(url))    
-    print("Downloading pdf")
-    print(r.status_code)
-    # print(r.content)
-    key = md5(r.content).hexdigest()
-
-    # Create a Cloud Storage client.
-    gcs = storage.Client()
-    name = key+'.json'
-
-    # Get the bucket that the file will be uploaded to.
-    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-    # Check if the file already exists
-    if bucket.blob(name).exists():
-        print("File already exists")
-        print("Done processing pdf")
-        return {"key": key}
-
+    r = requests.get(str(url))
+    print(r.headers)
     pdf = PdfReader(BytesIO(r.content))
-    paper_text = chatbot.extract_text(pdf)
-    df = chatbot.create_df(paper_text)
-    df = chatbot.embeddings(df)
-
-    # Create a new blob and upload the file's content.
-    blob = bucket.blob(name)
-    blob.upload_from_string(df.to_json(), content_type='application/json')
+    paper_text = chatbot.parse_paper(pdf)
+    global df
+    df = chatbot.paper_df(paper_text)
+    df = chatbot.calculate_embeddings(df)
     print("Done processing pdf")
-    return {"key": key}
+    return {'key': ''}
 
 @app.route("/reply", methods=['POST'])
 def reply():
     chatbot = Chatbot()
-    key = request.json['key']
     query = request.json['query']
     query = str(query)
-    print(query)
-    # df = pd.read_json(BytesIO(db.get(key)))
-    gcs = storage.Client()
-    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-    blob = bucket.blob(key+'.json')
-    df = pd.read_json(BytesIO(blob.download_as_string()))
-    print(df.head(5))
     prompt = chatbot.create_prompt(df, query)
     response = chatbot.gpt(prompt)
     print(response)
     return response, 200
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
