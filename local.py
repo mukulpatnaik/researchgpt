20b84711424f1ea7c66651fc9cabf06ea81b14a4
MrPeterJin <Cheng.Jin@std.uestc.edu.cn>
2023-03-18 12:02:08 +0800

    Railway port settings update

--- local.py
+++ local.py
@@ -451,4 +451,4 @@ def catch_all(path):
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=os.getenv("PORT"), debug=True)
