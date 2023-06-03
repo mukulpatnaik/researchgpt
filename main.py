bbe14b3cea43c7523100ae36f735ce9082a3c08a
mukul <mukulpatnaik@gmail.com>
2023-06-03 19:25:03 -0400

    clean up

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import os
 import requests
 import redis
 from _md5 import md5
-import cohere
 
 app = FastAPI()
 
@@ -79,7 +78,6 @@ class Chatbot():
                     blob_text = t["text"]
             paper_text += processed_text
         print("Done parsing paper")
-        # print(paper_text)
         return paper_text
 
     def create_df(self, data):
@@ -87,7 +85,6 @@ class Chatbot():
         if type(data) == list:
             print("Extracting text from pdf")
             print("Creating dataframe")
-            print(type(data))
             filtered_pdf = []
             # print(pdf.pages[0].extract_text())
             for row in data:
@@ -95,7 +92,6 @@ class Chatbot():
                     continue
                 filtered_pdf.append(row)
             df = pd.DataFrame(filtered_pdf)
-            print(df.columns)
             # remove elements with identical df[text] and df[page] values
             df = df.drop_duplicates(subset=["text", "page"], keep="first")
             # df['length'] = df['text'].apply(lambda x: len(x))
@@ -125,23 +121,19 @@ class Chatbot():
         results = df.sort_values("similarity", ascending=False, ignore_index=True)
         # make a dictionary of the the first three results with the page number as the key and the text as the value. The page number is a column in the dataframe.
         results = results.head(n)
-        print(results)
         sources = []
         for i in range(n):
             # append the page number and the text as a dict to the sources list
             sources.append({"Page " + str(results.iloc[i]["page"]): results.iloc[i]["text"][:150] + "..."})
-        print(sources)
         return {"results": results, "sources": sources}
 
     def create_prompt(self, df, user_input):
         print('Creating prompt')
         print(user_input)
-        print(df)
 
         result = self.search(df, user_input, n=3)
         data = result['results']
         sources = result['sources']
-        print(data)
         system_role = """You are a AI assistant whose expertise is reading and summarizing scientific papers. You are given a query, 
         a series of text embeddings and the title from a paper in order of their cosine similarity to the query. 
         You must take the given embeddings and return a very detailed summary of the paper in the languange of the query:
@@ -155,8 +147,6 @@ class Chatbot():
         3.""" + str(data.iloc[2]['text']) + """
         """
 
-        print(system_role)
-
         history = [
         {"role": "system", "content": system_role},
         {"role": "user", "content": str(user_input)}]
@@ -165,12 +155,11 @@ class Chatbot():
         return {'messages': history, 'sources': sources}
 
     def gpt(self, context, source):
-        print(context)
-        print('Sending request to GPT-3')
+        print('Sending request to OpenAI')
         openai.api_key = os.getenv('OPENAI_API_KEY')
         r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=context)
         answer = r.choices[0]["message"]["content"]
-        print('Done sending request to GPT-3')
+        print('Done sending request to OpenAI')
         response = {'answer': answer, 'sources': source}
         return response
 
@@ -178,23 +167,9 @@ class Chatbot():
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-def cohere_search(query, docs):
-    co = cohere.Client('MIT9tAqimYrMfFrBmSsHgBN2EaQc2inGKH9IQziY')
-    
-    response = co.rerank(
-    model = 'rerank-english-v2.0',
-    query = query,
-    documents = docs,
-    top_n = 3,
-    )
-
-    print(response)
-    return response
-
 @app.post("/process_pdf")
 async def process_pdf(request: Request):
     print("Processing pdf")
-    print(request)
     body = await request.body()
     key = md5(body).hexdigest()
     print(key)
@@ -213,9 +188,6 @@ async def process_pdf(request: Request):
 
     if db.get(key) is None:
         db.set(key, df.to_json())
-
-    # print(db.set(key, df.to_json()))
-    # print(db.get(key))
 
     print("Done processing pdf")
     return JSONResponse({"key": key})
